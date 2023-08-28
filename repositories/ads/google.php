685d7af046124da33d0a231340a7cd00bba9ee3b
Rudder <ben@ruddernation.com>
2023-08-29 00:39:13 +0100

    Add files via upload

---
+++ repositories/ads/google.php
@@ -0,0 +1,12 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5600679161903996"
+     crossorigin="anonymous"></script>
+<!-- RAF NEW -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5600679161903996"
+     data-ad-slot="9946587643"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
---
+++ repositories/index.php
@@ -0,0 +1,102 @@
+<?php if (function_exists('header_remove')) {
+    header_remove('X-Powered-By'); // PHP 5.3+
+	header_remove('Public-Key-Pins');
+	
+} else {
+    @ini_set('expose_php', 'off');
+	//@ini_set('ServerSignature', 'off');
+}
+?>
+<!DOCTYPE html>
+ <html>
+ <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Ruddernation&trade; Designs - Repositories</title>
+    <meta name="description" content="Ruddernation Designs" />
+		<meta name="keywords" content="ruddernation,tinychat,tinychat room spy,text converter,HTML5 canvas,HTML5,Ruddernation Designs" />
+		<meta name="author" content="Ruddernation Designs" />
+		<meta name="Copyright" content="Ruddernation Designs"/>
+	 <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="3dcafc0b-185e-4f5d-bb5f-0efa3cbe6b7d" type="text/javascript" async></script>
+    <link rel="stylesheet" href="css/syntax.css" type="text/css" />
+    <link rel="stylesheet" href="css/pygments.css" type="text/css" />
+    <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen, projection" />
+    <script src="js/jquery.js" type="text/javascript" charset="utf-8"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+ </head>
+ <body>
+<div class="site">
+	 <h1><a href="/" target="_blank" title="Return to the home page.">Ruddernation Designs</a></h1>
+  <div class="title">
+    <h2><a href="https://github.com/Ruddernation-Designs" target="_blank" title="GitHub Repositories">Ruddernation Designs - GitHub Repositories.</a></h2>
+  </div>
+<link rel="stylesheet" href="css/base.css">
+<div class="projects">
+	<div data-repo="Ruddernation-Designs/Ruddernation-Designs" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Tinychat-API" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/pyTelegramBotAPI" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/radixdlt-scrypto" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Password-Generator-JavaScript" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Userplane-Webchat" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Userplane-Webchat-2" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Userplanes-WpePro-Font-Filters" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Canvas-Effects" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Adobe-URL-Block-List" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/telegram-bot-core-sqlite" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/JavaScript-Colour-changers" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Tinychat-API-WordPress" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Programming_Books" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Userplane-for-WordPress" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Dummies-PDF-books" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/php-visitor-counter" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Simple-popup-contact-form" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/html5-contact-form-with-Google-maps" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/video-background-using-html5" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Random-Trivia-QandA" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Matrix" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Ad-Blocker-Checker" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/WannaCry-Decompiled" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Tinychat-Video-Chat-WordPress" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Ezcape-Video-Chat-WordPress" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Tinychat-Directory" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/python-cheat-sheet" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Group-Chat-for-BuddyPress" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Programming_Books" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Ethical_Hacking_Video_Course" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/Tinychat-API" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/jumpinchat-homepage" class="github-box-wrap"></div>
+	<div data-repo="Ruddernation-Designs/jumpinchat-web" class="github-box-wrap"></div>
+	</div>
+	<br>
+	<!-- Google Ads -->
+					<?php include "./../ads/google.html" ?>
+<!-- Google Ads End -->
+	<h1><a href="https://github.com/Ruddernation" target="_blank" title="Ruddernation on GitHub.">Ruddernation Repositories.</a></h1><br>
+	<div data-repo="Ruddernation/Website-Theme" class="github-box-wrap"></div>
+	<h1><a href="https://github.com/Tinychat" target="_blank" title="Return to the home page.">Tinychat Repositories.</a></h1><br>
+	<div data-repo="TinyChat/Tinychat-API" class="github-box-wrap"></div>
+	<div data-repo="TinyChat/Random-Tinychat-Scripts" class="github-box-wrap"></div>
+	<div data-repo="TinyChat/Tinychat-Bot--Discontinued" class="github-box-wrap"></div>
+	<div data-repo="TinyChat/Tinychat-Bot-Minimal--Discontinued" class="github-box-wrap"></div>
+	<div data-repo="TinyChat/Tinychats-Extracted-swf-Files-Final-Chat" class="github-box-wrap"></div>
+	<div data-repo="TinyChat/Tinychat-Video-Chat-WordPress" class="github-box-wrap"></div>
+	<div data-repo="TinyChat/Tinychat-WebRTC-app-prettified" class="github-box-wrap"></div>
+	<div data-repo="TinyChat/Tinychat-Directory" class="github-box-wrap"></div>
+	<div data-repo="Tinychat/Group-Chat-for-BuddyPress" class="github-box-wrap"></div>
+	
+	</div>
+ <script src="js/git.js"></script>
+ <script>
+ 	$('[data-repo]').github();
+ </script>
+<!-- Google Ads -->
+					<?php include "./../ads/google.html" ?>
+<!-- Google Ads End -->
+	 </div>
+	 <!-- Footer section start -->
+<div class="footer"><p><a href="/" class="footer" title="Ruddernation Designs">Ruddernation&trade; Designs <span id="current_year"></span></a>
+                <script type="text/javascript">
+                    document.getElementById('current_year').innerHTML = new Date().getFullYear();
+                </script> | <a href="/cookie-policy" class="footer" title="Cookie Policy">Cookie Policy</a> | <a href="/privacy-policy" class="footer" title="Privacy Policy">Privacy Policy</a> | <a href="/tac" class="footer" title="Terms and Conditions">Terms and Conditions</a></p></div>
+<!-- Footer section end -->
+</body>
+</html>
